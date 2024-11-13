cdd9d7b46521a093fba826487ebd9330d35d3c4c
amiraghajan78 <amiraghajan78@gmail.com>
2024-11-13 10:12:09 +0330

    create js file and add to project with js code

---
+++ script.js
@@ -0,0 +1,11 @@
+const $ = document;
+
+let userName = 'Amir';
+
+function isLogin(user) {
+    if (user.userName === 'Amir') {
+        return true;
+    } else {
+        return false;
+    };
+};
